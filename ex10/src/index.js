889acfb2b534d1a38ef6bbf9147903527af96bfa
merBec <becic.merjema@gmail.com>
2021-09-16 20:20:05 +0200

    Added ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+  var myFirstName = "Merjema";
+  var myLastName = "Becic";
+  var myHomeTown = "Sarajevo";
+
+  return {
+    myFirstName,
+    myLastName,
+    myHomeTown
+  };
+}
+console.log(main());
+module.exports = main;
